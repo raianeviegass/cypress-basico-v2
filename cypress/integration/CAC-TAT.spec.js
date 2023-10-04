dfa5dffb6a7481da5eab1a2156cc17de5256ebd2
Raiane Viegas <raianeviegass@gmail.com>
2023-10-04 17:25:05 -0300

    Adicionando mais testes

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -13,20 +13,20 @@ describe('Central de Atendimento ao Cliente TAT', function() {
 
     it('preenche os campos obrigatórios e envia o formulário', function() {
         const longText = 'Test, test, Test, test, Test, test, Test, test, Test, test, Test, test, Test, test, Test, test, Test, test'
-        cy.get('input[id="firstName"]').should('be.visible').type('Rai')
-        cy.get('input[id="lastName"]').should('be.visible').type('Test')
-        cy.get('input[id="email"]').should('be.visible').type('raitest@email.com')
-        cy.get('textarea[id="open-text-area"]').should('be.visible').type(longText, {delay:0})
-        cy.get('button[type="submit"]').should('be.visible').click()
+        cy.get('input[id="firstName"]').type('Rai')
+        cy.get('input[id="lastName"]').type('Test')
+        cy.get('input[id="email"]').type('raitest@email.com')
+        cy.get('textarea[id="open-text-area"]').type(longText, {delay:0})
+        cy.contains('button', 'Enviar').click()
         cy.get('span[class="success"]').should('be.visible')
     })
 
     it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function() {
-        cy.get('input[id="firstName"]').should('be.visible').type('Rai')
-        cy.get('input[id="lastName"]').should('be.visible').type('Test')
-        cy.get('input[id="email"]').should('be.visible').type('raitest@email,com')
-        cy.get('textarea[id="open-text-area"]').should('be.visible').type('Test')
-        cy.get('button[type="submit"]').should('be.visible').click()
+        cy.get('input[id="firstName"]').type('Rai')
+        cy.get('input[id="lastName"]').type('Test')
+        cy.get('input[id="email"]').type('raitest@email,com')
+        cy.get('textarea[id="open-text-area"]').type('Test')
+        cy.contains('button', 'Enviar').click()
         cy.get('span[class="error"]').should('be.visible')
     })
 
@@ -37,12 +37,12 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     })
     
     it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', function() {
-        cy.get('input[id="firstName"]').should('be.visible').type('Rai')
-        cy.get('input[id="lastName"]').should('be.visible').type('Test')
-        cy.get('input[id="email"]').should('be.visible').type('raitest@email.com')
-        cy.get('textarea[id="open-text-area"]').should('be.visible').type('Test')
-        cy.get('#phone-checkbox').click()
-        cy.get('button[type="submit"]').should('be.visible').click()
+        cy.get('input[id="firstName"]').type('Rai')
+        cy.get('input[id="lastName"]').type('Test')
+        cy.get('input[id="email"]').type('raitest@email.com')
+        cy.get('textarea[id="open-text-area"]').type('Test')
+        cy.get('#phone-checkbox').check()
+        cy.contains('button', 'Enviar').click()
         cy.get('span[class="error"]').should('be.visible')         
     })
 
@@ -69,9 +69,84 @@ describe('Central de Atendimento ao Cliente TAT', function() {
           .should('have.value', '')     
     })
 
-    it.only('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', function() {
-        cy.get('button[type="submit"]').should('be.visible').click()
+    it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', function() {
+        cy.contains('button', 'Enviar').click()
         cy.get('span[class="error"]').should('be.visible')
     })
 
+    it('envia o formuário com sucesso usando um comando customizado', function() {
+      cy.fillMandatoryFieldsAndSubmit()
+
+      cy.get('span[class="success"]').should('be.visible')
+    })
+
+    it('seleciona um produto (YouTube) por seu texto', function() {
+      cy.get('#product')
+        .select('YouTube')
+        .should('have.value', 'youtube')
+    })
+
+    it('seleciona um produto (Mentoria) por seu valor (value)', function() {
+      cy.get('#product')
+        .select('mentoria')
+        .should('have.value', 'mentoria')
+    })
+
+    it('seleciona um produto (Blog) por seu índice', function() {
+      cy.get('#product')
+        .select(1)
+        .should('have.value', 'blog')
+    })
+
+    it('marca o tipo de atendimento "Feedback"', function() {
+      cy.get('input[type="radio"][value="feedback"]')
+        .check()
+        .should('have.value', 'feedback')
+    })
+
+    it('marca cada tipo de atendimento', function() {
+      cy.get('input[type="radio"]')
+        .should('have.length', 3)
+        .each(function($radio) {
+          cy.wrap($radio).check()
+          cy.wrap($radio).should('be.checked')
+        })
+    })
+
+    it('marca ambos checkboxes, depois desmarca o último', function() {
+      cy.get('input[type="checkbox"]')
+        .check()
+        .should('be.checked')
+        .last()
+        .uncheck()
+        .should('not.be.checked')      
+    })
+
+    it('seleciona um arquivo da pasta fixtures', function() {
+      cy.get('#file-upload')
+        .should('not.have.value')
+        .selectFile('./cypress/fixtures/example.json')
+        .should($input => {
+          expect($input[0].files[0].name).to.equal('example.json')
+        })
+    })
+
+    it('seleciona um arquivo simulando um drag-and-drop', function() {
+      cy.get('#file-upload')
+        .should('not.have.value')
+        .selectFile('./cypress/fixtures/example.json', {action : 'drag-drop'})
+        .should($input => {
+          expect($input[0].files[0].name).to.equal('example.json')
+        })
+    })
+
+    it.only('seleciona um arquivo utilizando uma fixture para a qual foi dada um alias', function() {
+      cy.fixture('example.json').as('sampleFile')
+      cy.get('#file-upload')
+        .should('not.have.value')
+        .selectFile('@sampleFile')
+        .should($input => {
+          expect($input[0].files[0].name).to.equal('example.json')
+        })
+      })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,8 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('fillMandatoryFieldsAndSubmit', function () {
+        cy.get('input[id="firstName"]').type('Rai')
+        cy.get('input[id="lastName"]').type('Test')
+        cy.get('input[id="email"]').type('raitest@email.com')
+        cy.get('textarea[id="open-text-area"]').type('Test')
+        cy.contains('button', 'Enviar').click()
+
+})
